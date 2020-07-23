8dda97db07d685e5f5ca4ce5e8b9cbfcb9ffe9f7
afikrico <afikrico@gmail.com>
2020-07-23 22:20:57 +0700

    Add error handling message

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -8,16 +8,28 @@ items = []
 
 class Items(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        #for item in items:
+            #if item['name'] == name:
+             #   return item
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        #return {'item': None}, 404
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item:
+            return {"message": "An Item with name '{}' already exists.".format(name)}, 400
+
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
-api.add_resource(Items, '/item/<string:name>')
+class ItemList(Resource):
+    def get(self):
+        return {'item': items}
 
-app.run(port=5000)
+api.add_resource(Items, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
