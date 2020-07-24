32344403508280f5c9b1ef1d840fb557739ed398
afikrico <afikrico@gmail.com>
2020-07-24 15:31:28 +0700

    Full code from section 4

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
-app.secret_key = 
+app.secret_key = 'jose'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)  #/auth
 
 items = []
 
 class Items(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help='This field cannot be left blank!'
+    )
+
+    @jwt_required()
     def get(self, name):
         #for item in items:
             #if item['name'] == name:
@@ -16,15 +29,34 @@ class Items(Resource):
         #return {'item': None}, 404
         return {'item': item}, 200 if item else 404
 
+    @jwt_required()
     def post(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
+
         if item:
             return {"message": "An Item with name '{}' already exists.".format(name)}, 400
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
+        #data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x:x['name'] != name, items), None)
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args() #request.get_json()
+        item = next(filter(lambda x:x['name'] == name, items), None)
+        if item:
+            item.update(data)
+        else:
+            item = {'name':name, 'price':data['price']}
+        
+        items.append(item)
+        return item
 
 class ItemList(Resource):
     def get(self):
